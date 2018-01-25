20a264ddc2f130a849575d28d644a5be2812420b
ikramw <ikramw@hotmail.com>
2018-01-25 12:39:43 +0100

    styling nav

--- script/script.js
+++ script/script.js
@@ -1,4 +1,6 @@
 var nav=document.getElementById("nav-res");
+var nav_des=document.getElementById("nav-des");
+var div_hamburger=document.getElementById("nav_des");
 var icon=document.getElementById("hamburger");
 if (icon.addEventListener) {
     icon.addEventListener("click", show_menu, false);
@@ -15,4 +17,8 @@ function show_menu(){
 }
 nav.addEventListener("click",function(event){
 nav.className="displayNone";
+});
+
+nav_des.addEventListener("click",function(event){
+    nav_des.className="displayNone";
 });
